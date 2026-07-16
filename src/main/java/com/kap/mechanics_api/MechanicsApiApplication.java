80b725fdb7dacf78693725fbbe6aeaaf8248b5e6
Joao Pedro <joao.albuquerque@gruporas.com.br>
2026-07-16 11:03:40 -0300

    first commit

---
+++ src/main/java/com/kap/mechanics_api/MechanicsApiApplication.java
@@ -0,0 +1,13 @@
+package com.kap.mechanics_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MechanicsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MechanicsApiApplication.class, args);
+	}
+
+}
