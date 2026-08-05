57fa0531288e00b26cacb4f1c4468ed9fabc903c
pedro albuquerque <joaoalbuquerque.dev@gmail.com>
2026-08-04 21:16:05 -0300

    test: adicionando testes do service de veiculos

--- src/test/java/com/kap/mechanics_api/veiculo/VeiculoServiceTest.java
+++ src/test/java/com/kap/mechanics_api/veiculo/VeiculoServiceTest.java
@@ -4,6 +4,7 @@ import com.kap.mechanics_api.domain.Veiculo;
 import com.kap.mechanics_api.mapper.VeiculoMapper;
 import com.kap.mechanics_api.repository.VeiculoRepository;
 import com.kap.mechanics_api.service.VeiculoService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -11,9 +12,14 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import com.kap.mechanics_api.dto.veiculo.*;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -49,5 +55,120 @@ public class VeiculoServiceTest {
         assertNotNull(resultado);
         assertEquals("ABC1234", resultado.placa());
     }
+
+    @Test
+    void deveListarTodosOsVeiculos(){
+
+        //Arrange
+        Veiculo veiculoSalvo = new Veiculo(1, "ABC1234", "Honda", "Civic", 2020);
+        Veiculo veiculoSalvo2 = new Veiculo(2, "ADD1234", "Honda", "CRV", 2022);
+        List<Veiculo> veiculos = new ArrayList<>();
+        veiculos.add(veiculoSalvo);
+        veiculos.add(veiculoSalvo2);
+
+        List<ListagemVeiculoResponseDTO> veiculosDto = new ArrayList<>();
+        ListagemVeiculoResponseDTO dto1 = new ListagemVeiculoResponseDTO(1, "ABC1234", "Honda", "Civic", 2020);
+        ListagemVeiculoResponseDTO dto2 = new ListagemVeiculoResponseDTO(2, "ADD1234", "Honda", "CRV", 2022);
+        veiculosDto.add(dto1);
+        veiculosDto.add(dto2);
+
+        when(veiculoRepository.findAll()).thenReturn(veiculos);
+        when(veiculoMapper.toListagemDto(veiculos)).thenReturn(veiculosDto);
+
+
+        //act
+        List<ListagemVeiculoResponseDTO> resultado = veiculoService.listar();
+
+        //Assert
+        assertNotNull(resultado);
+        assertEquals(2, resultado.size());
+
+        assertEquals(1, resultado.get(0).id());
+        assertEquals("ABC1234", resultado.get(0).placa());
+        assertEquals("Honda", resultado.get(0).marca());
+        assertEquals("Civic", resultado.get(0).modelo());
+        assertEquals(2020, resultado.get(0).ano());
+
+        assertEquals(2, resultado.get(1).id());
+        assertEquals("ADD1234", resultado.get(1).placa());
+
+        verify(veiculoRepository).findAll();
+        verify(veiculoMapper).toListagemDto(veiculos);
+
+    }
+
+
+    @Test
+    void deveBuscarVeiculoPorId(){
+
+        //arrange
+        Optional<Veiculo> veiculo = Optional.of(new Veiculo(1, "ABC1234", "Honda", "Civic", 2020));
+        ListagemVeiculoResponseDTO dto1 = new ListagemVeiculoResponseDTO(1, "ABC1234", "Honda", "Civic", 2020);
+
+        when(veiculoRepository.findById(1)).thenReturn(veiculo);
+        when(veiculoMapper.toListagemVeiculoResponseDto(veiculo.get())).thenReturn(dto1);
+
+        //act
+        ListagemVeiculoResponseDTO resultado = veiculoService.buscarPorId(veiculo.get().getId());
+
+        //assert
+        assertNotNull(resultado);
+        assertEquals(1, resultado.id());
+        assertEquals("ABC1234", resultado.placa());
+        assertEquals("Honda", resultado.marca());
+        assertEquals("Civic", resultado.modelo());
+        assertEquals(2020, resultado.ano());
+
+        verify(veiculoRepository).findById(1);
+        verify(veiculoMapper).toListagemVeiculoResponseDto(veiculo.get());
+
+    }
+
+    @Test
+    void deveDeletarVeiculoComSucesso(){
+
+        //arrange
+        Veiculo veiculo = new Veiculo(1, "ABC1234", "Honda", "Civic", 2020);
+        when(veiculoRepository.findById(1)).thenReturn(Optional.of(veiculo));
+
+        //act
+        veiculoService.deletar(veiculo.getId());
+
+        //assert
+        verify(veiculoRepository).findById(1);
+        verify(veiculoRepository).delete(veiculo);
+
+    }
+
+    @Test
+    void deveAtualizarVeiculoComSucesso(){
+
+        //arrange
+        Veiculo veiculo = new Veiculo(1, "ABC1234", "Honda", "Civic", 2020);
+        AtualizacaoVeiculoRequestDTO dto = new AtualizacaoVeiculoRequestDTO("ABC1234", "Honda2", "Civic2", 2020);
+        Veiculo veiculoAlterado = new Veiculo(1, "ABC1234", "Honda2", "Civic2", 2020);
+        AtualizacaoVeiculoResponseDTO dtoResposta = new AtualizacaoVeiculoResponseDTO(1, "ABC1234", "Honda2", "Civic2", 2020);
+
+        when(veiculoRepository.findById(1)).thenReturn(Optional.of(veiculo));
+        when(veiculoMapper.toAtualizacaoVeiculoResponseDto(veiculoAlterado)).thenReturn(dtoResposta);
+        when(veiculoRepository.save(any(Veiculo.class))).thenReturn(veiculoAlterado);
+
+        //act
+        AtualizacaoVeiculoResponseDTO resposta = veiculoService.atualizar(dto, veiculo.getId());
+
+
+        //assert
+        assertNotNull(resposta);
+        assertEquals(1, resposta.id());
+        assertEquals("ABC1234", resposta.placa());
+        assertEquals("Honda2", resposta.marca());
+        assertEquals("Civic2", resposta.modelo());
+        assertEquals(2020, resposta.ano());
+
+        verify(veiculoRepository).findById(1);
+        verify(veiculoMapper).toAtualizacaoVeiculoResponseDto(veiculoAlterado);
+        verify(veiculoRepository).save(veiculo);
+    }
+
 
 }
