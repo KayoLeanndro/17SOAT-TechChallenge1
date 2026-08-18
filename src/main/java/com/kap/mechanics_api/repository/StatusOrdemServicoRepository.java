701740bb5f40aa53aeca676aeddd98156d11f808
João Albuquerque <69771492+joaoalbuquerq@users.noreply.github.com>
2026-08-18 09:45:07 -0300

    Revert "feat: CRUD OS"

--- src/main/java/com/kap/mechanics_api/repository/StatusOrdemServicoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.kap.mechanics_api.repository;
-
-import com.kap.mechanics_api.domain.StatusOrdemServico;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface StatusOrdemServicoRepository extends JpaRepository<StatusOrdemServico, Integer> {
-}
