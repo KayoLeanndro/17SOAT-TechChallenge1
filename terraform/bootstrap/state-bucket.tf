1ec023f3a81645271fbe40255ecd79bbee1666d4
Kayo Leanndro <100872126+KayoLeanndro@users.noreply.github.com>
2026-09-21 23:28:30 -0300

    chore: inicializar provider AWS do Terraform

--- terraform/bootstrap/state-bucket.tf
+++ terraform/bootstrap/state-bucket.tf
@@ -19,7 +19,7 @@ resource "aws_s3_bucket_public_access_block" "terraform_state" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-  
+
 }
 
 resource "aws_s3_bucket_versioning" "terraform_state" {
